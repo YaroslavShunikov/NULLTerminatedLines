84520890f06dc62580779b369eaacc57159857dd
Ярослав Шуников <gabriel.yar2001@gmail.com>
2023-03-21 22:55:16 +0300

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<windows.h>
+#include <string>
+using namespace std;
+#define delimiter "-------------------------------------------------------------------------------------"
+
+int StringLenght(const char* str);
+void to_upper(char str[]);              //Перевод в верхний регистр
+void to_lower(char str[]);				//Перевод в нихижний регистр 
+void shrink(char str[]);				//удаляет лишние пробелы из строки
+//bool is_palindrome(const char str[]);
+bool is_int_number(const char str[]);
+int to_int_number(const char str[]);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	/*char str[] = {"Hello"};
+	cout << str << endl;*/
+
+	const int n = 256;
+	char str[n] = {};
+	cout << "Введите строку: "; //cin >>str ;
+	SetConsoleCP(1251);
+	cin.getline(str, n);
+	SetConsoleCP(866);
+	cout << str << endl;
+	cout << "Размер строки: " << StringLenght(str) << endl;
+	to_upper(str);
+	cout << str << endl << endl;
+	to_lower(str);
+	cout << str << endl << endl;
+	shrink(str);
+	cout << str<< endl<<endl;
+	cout<<"Строка" << (is_int_number(str)?" ":"Не")<<"десятичное число" << endl;
+	to_int_number(str);
+}
+
+int StringLenght(const char* str)
+{
+	int i = 0;
+	for (; str[i]; i++);
+	return i;
+}
+void to_upper(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')str[i] -= 32;
+		if (str[i] >= 'а' && str[i] <= 'я')str[i] -= 32;
+	}
+}
+void to_lower(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')str[i] += 32;
+		if (str[i] >= 'А' && str[i] <= 'Я')str[i] += 32;
+	}
+}
+void shrink(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == 32 && str[i + 1] == 32)
+		{
+			//цикл сдвига фрагмента массива
+			for (int j = i; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+		}
+	}
+}
+bool is_int_number(const char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] < '0' || str[i]>'9') return false;
+	}
+	return true;
+}
+int to_int_number(const char str[])
+{
+	
+	if (is_int_number(str) == true)
+	{
+		int n = StringLenght(str);
+		int value=((int)str[0]-48);
+		int walle;
+		int eva = 1;
+		for (int i = n-1; i>0; i--)
+		{
+			value *= 10;
+			
+		}
+		value+= ((int)str[n-1] - 48);
+		for (int i = n - 2; i > 0; i--)
+		{
+			walle = ((int)str[i]-48)*pow(10,eva);
+			value += walle;
+
+			eva++;
+		}
+
+		cout << value << endl;
+	}
+	else return 0;
+}
